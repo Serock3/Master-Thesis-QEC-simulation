1f6887998e82c27d479ec0912e2f8699a0888c7a
Sebastian <sebastian.holmin@hotmail.com>
2021-02-12 11:14:13 +0100

    Auto stash before merge of "master" and "origin/master"

--- simulator_program/stabilizer_testing_seb.py
+++ simulator_program/stabilizer_testing_seb.py
@@ -1,6 +1,7 @@
 #%%
-from stabilizers import measure_stabilizer
-from qiskit.circuit.quantumregister import QuantumRegister
+# from stabilizers import measure_stabilizer
+from qiskit import execute, Aer
+from qiskit.circuit.quantumregister import QuantumRegister, AncillaRegister
 from qiskit.circuit.classicalregister import ClassicalRegister
 from qiskit.circuit.quantumcircuit import QuantumCircuit
 import numpy as np
@@ -54,76 +55,71 @@ def encode_input(qbReg):
     return encoding_circuit
 qbReg = QuantumRegister(5, 'code_qubit')
 # The two ancilla qubits (one of them is unused)
-anReg = QuantumRegister(2, 'ancilla_qubit')
-clReg = ClassicalRegister(5, 'syndrome_bit')
+anReg = QuantumRegister(1, 'ancilla_qubit')
+flagQbReg = QuantumRegister(1, 'flag_qubit')
+clReg = ClassicalRegister(4, 'syndrome_bit')
 # Flags
-flag = ClassicalRegister(1, 'flag_bit')
+flagBtReg = ClassicalRegister(1, 'flag_bit')
 # Readout of the final state at the end for statistics
-readout = ClassicalRegister(5, 'readout')
 #%%
-circ = QuantumCircuit(clReg, readout, qbReg, anReg) #, flag,
-
+circ = QuantumCircuit(clReg,flagBtReg, qbReg, anReg, flagQbReg) #, flag,
+reset = True
 # circ += encode_input(qbReg)
 # circ.snapshot_statevector('post_encoding')
 circ.initialize(logical_0, qbReg)
 circ.barrier()
-# X
+
+# Noise test
+# circ.z( qbReg[0] ) 
+
+# X, a
 circ.h( qbReg[0] ) # X
-circ.h( anReg[1] )
-circ.cz( anReg[1], qbReg[0] )
+circ.h( anReg[0] )
+circ.cz( anReg[0], qbReg[0] )
 circ.h( qbReg[0] )
 
 # Flag
-circ.h( anReg[0] ) # Initialize in +
-circ.cz( anReg[0], anReg[1] ) # Flag CX        
+circ.h( flagQbReg[0] ) # Initialize in +
+circ.cz( flagQbReg[0], anReg[0] ) # Flag CX        
 
-# Z
-circ.cz(anReg[1], qbReg[1])
+# Z, b
+circ.cz(anReg[0], qbReg[1])
 
-# Z
-circ.cz( anReg[1], qbReg[2] )
+# Two qb noise test
+circ.x(anReg[0])
+
+# Z, c
+circ.cz( anReg[0], qbReg[2] )
 
 # Flag
-circ.cz( anReg[0], anReg[1] )   
+circ.cz( flagQbReg[0], anReg[0] )   
 
-# X
+# X, d
 circ.h(qbReg[3])
-circ.cz(anReg[1], qbReg[3])
-circ.h(anReg[1])
+circ.cz(anReg[0], qbReg[3])
+circ.h(anReg[0])
 circ.h(qbReg[3])
 
 # Measure
-circ.measure(anReg[1], clReg[0])
-circ.h(anReg[0])
-circ.measure(anReg[0], clReg[4])
-reset = True
+circ.measure(anReg[0], clReg[0])
+circ.h(flagQbReg[0])
+circ.measure(flagQbReg[0], flagBtReg[0])
 if reset:
-    circ.reset(anReg[1])
     circ.reset(anReg[0])
-
-# Run the stabilizers
-circ += measure_stabilizer(qbReg, anReg, clReg, 0, reset=True)
-circ += measure_stabilizer(qbReg, anReg, clReg, 1, reset=True)
-circ += measure_stabilizer(qbReg, anReg, clReg, 2, reset=True)
-circ += measure_stabilizer(qbReg, anReg, clReg, 3, reset=True)
-
-# Temporary recovery with flag
-circ.y(qbReg[2]).c_if(clReg, 1+16)
-circ.x(qbReg[3]).c_if(clReg, 1+16)
-circ.z(qbReg[2]).c_if(clReg, 2+16)
-circ.x(qbReg[3]).c_if(clReg, 2+16)
-circ.x(qbReg[1]).c_if(clReg, 3+16)
-circ.z(qbReg[2]).c_if(clReg, 3+16)
-circ.x(qbReg[3]).c_if(clReg, 3+16)
-circ.x(qbReg[2]).c_if(clReg, 5+16)
-circ.x(qbReg[3]).c_if(clReg, 5+16)
-circ.x(qbReg[3]).c_if(clReg, 6+16)
-circ.x(qbReg[0]).c_if(clReg, 8+16) # This seems equivalent with IZZXI
-circ.y(qbReg[1]).c_if(clReg, 9+16)
-circ.z(qbReg[2]).c_if(clReg, 9+16)
-circ.x(qbReg[3]).c_if(clReg, 9+16)
-circ.draw(output='mpl')
+    circ.reset(flagQbReg[0])
 #circ += recovery_scheme(...)
+
+#
+results = execute(
+    circ,
+    Aer.get_backend('qasm_simulator'),
+    noise_model=None,
+    shots=10
+).result()
+print(results.get_counts())
+
+#%%
+circ.draw()
 # %%
 print('circ depth = ',circ.depth())
 transpiled_circ = transpile(circ, optimization_level=2)
