d044b8ea765fada0faff36c1908c5e2c72971f2a
Sebastian Holmin <sebastian.holmin@hotmail.com>
2021-03-29 18:48:51 +0200

    my version of idle noise

--- trash/idle_noise_seb.py
+++ trash/idle_noise_seb.py
@@ -22,25 +22,34 @@ import numpy as np
 from qiskit import *
 
 from simulator_program.custom_noise_models import thermal_relaxation_model
+from qiskit.providers.aer.extensions.snapshot_density_matrix import *
+from simulator_program.custom_transpiler import shortest_transpile_from_distribution, WAQCT_device_properties
 # %% Temporary circuit for testing
 qb = QuantumRegister(3, 'code_qubit')
 an = AncillaRegister(2, 'ancilla_qubit')
 readout = ClassicalRegister(3, 'readout')
 
 circ = QuantumCircuit(qb, an, readout)
-
 circ.x(qb[1])
 circ.cx(qb[2], qb[1])
-# circ.iswap(qb[2],qb[1])
+circ.iswap(an[0],qb[2])
+circ.measure(an[0], readout[0])
+circ.x(qb[0]).c_if(readout,3)
 circ.barrier()
-circ.measure(qb[0], readout[0])
-circ.x(qb[0]).c_if(readout,1)
+
+circ.z(qb[0])
+circ.append(Snapshot('asd', "density_matrix", num_qubits=2), [qb[2],qb[0]])
 circ.draw()
 
-# %%
+circ_t = shortest_transpile_from_distribution(circ, print_cost=False, layout_method='sabre',
+        repeats=1, optimization_level=1, **WAQCT_device_properties)
+circ_t.draw()
+
+circ_t._layout
+# %% With idle noise
 # TODO: Expand gate_times. Probably want to have it as optional input?
 gate_durations = {'x': 20, 'z':0 ,'cx': 200,'cx': 200,'iswap': 200, 'barrier': 0, 'measure': 1000}  # times in ns
-dag = circuit_to_dag(circ)
+
 
 
 # From the BasicSwap example 
@@ -49,6 +58,9 @@ from qiskit.dagcircuit.dagcircuit import DAGCircuit
 from qiskit.circuit.library.standard_gates.h import CHGate
 
 # This is mostly a rewrite of the depth function with non-trivial time steps
+
+# def add_idle_noise(circ, include_gate_noise = False):
+dag = circuit_to_dag(circ_t)
 reg_offset = 0
 reg_map = {}
 # This can be done more elegantly with the OrderedDicts
@@ -57,23 +69,79 @@ for reg in list(dag.qregs.values()) + list(dag.cregs.values()):
     reg_offset += reg.size
 
 op_stack = [0] * reg_offset
-
 new_dag = dag._copy_circuit_metadata()
 
 for layer in dag.serial_layers():
     subdag = layer['graph']
     node = subdag.op_nodes()[0]
-    print(node.name)
-    print(node.qargs)
-    print(node.cargs)
-    print(node.condition)
-    print(' ')
+    # print(node.name)
+    # print(node.qargs)
+    # print(node.cargs)
+    # print(node.condition)
+    # print(' ')
 
     kraus_layer = DAGCircuit()
     for qreg in subdag.qregs.values():
         kraus_layer.add_qreg(qreg)
 
-    kraus_layer.apply_operation_back()
+    levels = []
+    reg_ints = []
+
+    duration = 0
+    try:
+        duration = gate_durations[node.name]
+    except KeyError as op:
+        print(f'No operation duration specified for {op.args}, assuming instant.')
+
+        
+    # Assuming here that there is no conditional
+    # snapshots or barriers ever.
+    if node.condition:
+        # The solution right now is to not include noise att all for classical conditionals
+        duration = 0
+        # Controls operate over all bits in the
+        # classical register they use.
+        # As opposed to measurements which only operate on one bit of the register.
+        # Multiple measurements on different bits may be done simultanously on the same register
+        cint = reg_map[node.condition[0].name]
+        
+        for off in range(node.condition[0].size):
+            if cint + off not in reg_ints:
+                reg_ints.append(cint + off)
+                levels.append(op_stack[cint + off])
+
+    for ind, reg in enumerate(node.qargs+node.cargs):
+        reg_ints.append(reg_map[reg.register.name] + reg.index)
+        levels.append(op_stack[reg_ints[-1]])
+
+    max_level = max(levels)
+
+
+    T1=40e3
+    T2=60e3
+    for qarg in node.qargs:
+        idle_time = max_level - op_stack[reg_map[qarg.register.name]+qarg.index]
+        if idle_time:
+            thrm_relax = thermal_relaxation_error(T1, T2, idle_time).to_instruction()
+            thrm_relax.name = f'Idle {idle_time}ns'
+            kraus_layer.apply_operation_back(thrm_relax, qargs=[qarg])
+    
+    for ind in reg_ints:
+        op_stack[ind] = max_level + duration
+
+    new_dag.compose(kraus_layer)
+    new_dag.compose(subdag)
+    # return new_dag
+
+print('total circuit time:', max(op_stack), 'ns')
+
+noisy_i = dag_to_circuit(new_dag)
+noisy_circ._layout = circ_t._layout
+noisy_circ.draw()
+#%%
+
+subdag.apply_operation_front(thermal_relaxation_error(T1, T2, time_to_next_op).to_instruction(), qargs=[qarg], cargs=[])
+dag.substitute_node_with_dag(node, subdag,wires=node.qargs)
 
 # May not work
 for node in dag.op_nodes():
