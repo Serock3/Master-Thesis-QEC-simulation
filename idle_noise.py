1cbfdc352573dda3de18733ec3041f847914b722
Sebastian Holmin <sebastian.holmin@hotmail.com>
2021-03-30 11:21:45 +0200

    Bugfix

--- idle_noise.py
+++ idle_noise.py
@@ -3,6 +3,7 @@
 # be called for related purposes, such as noise models.
 
 # %% Import modules
+from qiskit import QuantumCircuit
 from qiskit.providers.aer.noise import thermal_relaxation_error
 from qiskit.converters import circuit_to_dag
 import numpy as np
@@ -10,8 +11,10 @@ import numpy as np
 from simulator_program.custom_noise_models import thermal_relaxation_model
 
 # %%
-def add_idle_noise_to_circuit(circ, gate_times={}, T1=40e3, T2=60e3, 
-        return_time=False):
+
+
+def add_idle_noise_to_circuit(circ, gate_times={}, T1=40e3, T2=60e3,
+                              return_time=False):
     """Creates a copy of a circuit with added thermal relaxation noise added
     for idle qubits.
 
@@ -38,16 +41,16 @@ def add_idle_noise_to_circuit(circ, gate_times={}, T1=40e3, T2=60e3,
     new_circ = QuantumCircuit()
     for reg in circ.qregs + circ.cregs:
         new_circ.add_register(reg)
-    
+
     # Dictionary with the times for each snapshot
     time_at_snapshots_and_end = {}
 
     time_passed = {}
     for reg in circ.qubits + circ.clbits:
-         time_passed[reg] = 0
-    
+        time_passed[reg] = 0
+
     for node in dag.op_nodes():
-        # Set cargs to entire classical conditional register if it exists, otherwise to the cargs 
+        # Set cargs to entire classical conditional register if it exists, otherwise to the cargs
         cargs = node.condition[0] if node.condition else node.cargs
 
         # List of bits included in gate
@@ -55,15 +58,16 @@ def add_idle_noise_to_circuit(circ, gate_times={}, T1=40e3, T2=60e3,
         for arg in node.qargs+list(cargs):
             gate_args.append(arg)
 
-        latest_time = max(time_passed.values())
+        latest_time = max([time_passed[gate_arg] for gate_arg in gate_args ])
         # Apply idle noise to qargs
         for qarg in node.qargs:
-            time_diff= latest_time - time_passed[qarg]
+            time_diff = latest_time - time_passed[qarg]
             if time_diff:
-                thrm_relax = thermal_relaxation_error(T1, T2, time_diff).to_instruction()
+                thrm_relax = thermal_relaxation_error(
+                    T1, T2, time_diff).to_instruction()
                 thrm_relax.name = f'Idle {time_diff}ns'
-                new_circ.append(thrm_relax,[qarg])
-    
+                new_circ.append(thrm_relax, [qarg])
+
         # Assume instant if classical condition exists TODO: Better solution?
         gate_time = full_gate_times[node.name] if not node.condition else 0
         # Advance the time for the qubits included in the gate
@@ -71,7 +75,8 @@ def add_idle_noise_to_circuit(circ, gate_times={}, T1=40e3, T2=60e3,
             time_passed[gate_arg] = latest_time + gate_time
 
         if node.name == 'snapshot':
-            time_at_snapshots_and_end[node.op.label] = max(time_passed.values())
+            time_at_snapshots_and_end[node.op.label] = max(
+                time_passed.values())
 
         # Add the gate
         new_circ.append(node.op, node.qargs, node.cargs)
@@ -83,6 +88,7 @@ def add_idle_noise_to_circuit(circ, gate_times={}, T1=40e3, T2=60e3,
     if return_time:
         return new_circ, time_at_snapshots_and_end
     return new_circ
+
 
 def get_circuit_time(circ, gate_times):
     """Returns the total run time of a circuit, given specific gate times.
@@ -108,10 +114,10 @@ def get_circuit_time(circ, gate_times):
     time_at_snapshots_and_end = {}
     time_passed = {}
     for reg in circ.qubits + circ.clbits:
-         time_passed[reg] = 0
-    
+        time_passed[reg] = 0
+
     for node in dag.op_nodes():
-        # Set cargs to entire classical conditional register if it exists, otherwise to the cargs 
+        # Set cargs to entire classical conditional register if it exists, otherwise to the cargs
         cargs = node.condition[0] if node.condition else node.cargs
 
         gate_args = []
@@ -119,41 +125,46 @@ def get_circuit_time(circ, gate_times):
             gate_args.append(arg)
 
         latest_time = max(time_passed.values())
-    
+
         for gate_arg in gate_args:
             time_passed[gate_arg] = latest_time + full_gate_times[node.name]
 
         if node.name == 'snapshot':
-            time_at_snapshots_and_end[node.op.label] = max(time_passed.values())
+            time_at_snapshots_and_end[node.op.label] = max(
+                time_passed.values())
 
     time_at_snapshots_and_end['end'] = max(time_passed.values())
-    
+
     return time_at_snapshots_and_end
 
 # NOTE: Kanske bör ligga i custom_transpiler? Osäker
+
+
 def get_standard_gate_times():
     """Return a dict of standard gate times (ns) used for simulator purposes."""
     return {
         'x': 20, 'y': 20, 'z': 0, 'h': 20, 'u1': 0, 'u2': 20, 'u3': 20,
         'cx': 200, 'cz': 200, 'swap': 200, 'iswap': 200,
         'barrier': 0, 'measure': 500, 'snapshot': 0
-    } 
+    }
 
 # TODO: Detta kan väl vara ett argumment i funktionen ovan?
+
+
 def add_standard_gate_times(incomplete_gate_times={}):
     """Add the standard gate times to a dict with missing entries"""
     standard_gate_times = get_standard_gate_times()
     # TODO: Add an if-statement that checks for keys 'single' and 'double' in
-    #   incomplete list. If they exist, apply its value to all single/two qubit 
+    #   incomplete list. If they exist, apply its value to all single/two qubit
     #   gates instead of using standard times
     for key in standard_gate_times:
         if key not in incomplete_gate_times:
             incomplete_gate_times[key] = standard_gate_times[key]
     return incomplete_gate_times
 
+
 # %% Internal testing with a standard stabilizer circuit
 if __name__ == '__main__':
-    from qiskit import *
     from simulator_program.stabilizers import *
     from simulator_program.custom_transpiler import *
 
@@ -163,52 +174,62 @@ if __name__ == '__main__':
 
     circ = QuantumCircuit(qb, an, readout)
 
-    circ.x(qb[1])
-    circ.cx(qb[2], qb[1])
-    circ.iswap(an[0],qb[2])
+    # circ.x(qb[1])
+    # circ.cx(qb[2], qb[1])
+    # circ.iswap(an[0], qb[2])
     circ.measure(an[0], readout[0])
-    circ.x(qb[0]).c_if(readout,3)
-    circ.barrier()
+    circ.measure(an[1], readout[1])
+    circ.x(qb[0]).c_if(readout, 3)
+    # circ.barrier()
 
-    circ.z(qb[0])
-    circ.append(Snapshot('asd', "density_matrix", num_qubits=2), [qb[2],qb[0]])
-    circ.measure(an[0], readout[0])
+    # circ.z(qb[0])
+    # circ.append(Snapshot('asd', "density_matrix",
+    #                      num_qubits=2), [qb[2], qb[0]])
+    # circ.measure(an[0], readout[0])
 
     display(circ.draw())
-    new_circ, times = add_idle_noise_to_circuit(circ,return_time=True)
+    # circ_t = transpile(circ, routing_method='sabre', initial_layout=None,
+    #                    translation_method=None, layout_method='sabre',
+    #                    optimization_level=1, **WAQCT_device_properties)
+    # circ_t = shortest_transpile_from_distribution(circ, print_cost=False,
+    #                                               repeats=1, routing_method='sabre', initial_layout=None,
+    #                                               translation_method=None, layout_method='sabre',
+    #                                               optimization_level=1, **WAQCT_device_properties)
+    new_circ, times = add_idle_noise_to_circuit(circ, return_time=True)
     print(new_circ)
     print(times)
 
-    # flag = False
-    # reset = False
-    # recovery = True
-    # n_cycles=1
-    
-    # # Define our registers (Maybe to be written as function?)
-    # qb = QuantumRegister(5, 'code_qubit')
-    # an = AncillaRegister(2, 'ancilla_qubit')
-    # cr = get_classical_register(n_cycles, reset=reset, recovery=recovery, flag=flag)
-    # readout = ClassicalRegister(5, 'readout')
+    flag = False
+    reset = False
+    recovery = True
+    n_cycles=0
 
-    # registers = StabilizerRegisters(qb, an, cr, readout)
-    # circ = get_empty_stabilizer_circuit(registers)
+    # Define our registers (Maybe to be written as function?)
+    qb = QuantumRegister(5, 'code_qubit')
+    an = AncillaRegister(2, 'ancilla_qubit')
+    cr = get_classical_register(n_cycles, reset=reset, recovery=recovery, flag=flag)
+    readout = ClassicalRegister(5, 'readout')
 
-    # # Get the complete circuit
-    # circ += get_full_stabilizer_circuit(registers,
-    #                                     n_cycles=n_cycles,
-    #                                     reset=reset,
-    #                                     recovery=recovery,
-    #                                     flag=flag,
-    #                                     )
+    registers = StabilizerRegisters(qb, an, cr, readout)
+    circ = get_empty_stabilizer_circuit(registers)
 
-    # # Transpile
-    # circ_t = shortest_transpile_from_distribution(circ, print_cost=False,
-    #         repeats=10, routing_method='sabre', initial_layout=None,
-    #         translation_method=None, layout_method='sabre',
-    #         optimization_level=1, **WAQCT_device_properties)
+    # Get the complete circuit
+    circ += get_full_stabilizer_circuit(registers,
+                                        n_cycles=n_cycles,
+                                        reset=reset,
+                                        recovery=recovery,
+                                        flag=flag,
+                                        )
 
-    # new_circ, times = add_idle_noise_to_circuit(circ_t, return_time=True)
-    # # print(new_circ)
-    # new_circ.draw(output='mpl')
+    # Transpile
+    circ_t = shortest_transpile_from_distribution(circ, print_cost=False,
+            repeats=10, routing_method='sabre', initial_layout=None,
+            translation_method=None, layout_method='sabre',
+            optimization_level=1, **WAQCT_device_properties)
+
+    new_circ, times = add_idle_noise_to_circuit(circ_t, return_time=True)
+    print(new_circ)
+    print(times)
+    # display(new_circ.draw(output='mpl'))
 
 # %%
