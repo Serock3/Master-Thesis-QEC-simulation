23d6568261001aa327b3412ac4582a5948526249
Sebastian Holmin <sebastian.holmin@hotmail.com>
2021-05-14 12:13:53 +0200

    Looked at fidelity bound and projections

--- decay_comparison.py
+++ decay_comparison.py
@@ -11,11 +11,14 @@ from qiskit import Aer
 from simulator_program.stabilizers import add_snapshot_to_circuit, logical_states
 from qiskit.quantum_info.states.measures import state_fidelity
 import scipy
+from qiskit.quantum_info import Statevector
+from qiskit.quantum_info.operators import Operator
+from qiskit.providers.aer.noise import kraus_error
 # Settings to used across most configurations
 n_cycles = 5
 n_shots = 1
 
-
+# Statevector(logical_0)
 def get_idle_single_qubit(snapshot_times, snapshot_type='dm', T1=40e3, T2=60e3,
                           theta=0, phi=0, pauliop='Z'):
     """Generates a single qubit-circuit initialized in the |1> state with
@@ -41,7 +44,7 @@ def get_idle_single_qubit(snapshot_times, snapshot_type='dm', T1=40e3, T2=60e3,
         if time_diff > 0:
             thrm_relax = thermal_relaxation_error(
                 T1, T2, time_diff).to_instruction()
-            # print(time_diff,T1)
+
             circ.append(thrm_relax, [qb[0]])
         add_snapshot_to_circuit(circ, snapshot_type, i, [
                                 qb[0]], conditional=False, pauliop=pauliop)
@@ -70,6 +73,34 @@ def get_idle_encoded_513(snapshot_times, snapshot_type='dm', T1=40e3, T2=60e3,
             for qubit in circ.qubits:
                 circ.append(thrm_relax, [qubit])
 
+        add_snapshot_to_circuit(circ, snapshot_type, i,
+                                circ.qubits, conditional=False, pauliop=pauliop)
+        time_passed = time
+
+    simulator = Aer.get_backend('aer_simulator')
+    simulator.set_option('method', 'density_matrix')
+    results = execute(circ, simulator,
+                      noise_model=None, shots=n_shots).result()
+    return results
+
+def get_idle_projected_encoded_513(snapshot_times, snapshot_type='dm', T1=40e3, T2=60e3,
+                         theta=0, phi=0, pauliop='ZZZZZ'):
+    logical_0, logical_1 = logical_states(include_ancillas=None)
+    circ = QuantumCircuit(5)
+    initial_state = np.cos(theta/2)*logical_0 + \
+        np.exp(1j*phi)*np.sin(theta/2)*logical_1
+    # Projection operator
+    P_L = Statevector(logical_0).to_operator()+Statevector(logical_1).to_operator()
+    circ.set_density_matrix(initial_state)
+    time_passed = 0
+    for i, time in enumerate(snapshot_times):
+        time_diff = time-time_passed
+        if time_diff > 0:
+            thrm_relax = thermal_relaxation_error(
+                T1, T2, time_diff).to_instruction()
+            for qubit in circ.qubits:
+                circ.append(thrm_relax, [qubit])
+        circ.append(P_L)
         add_snapshot_to_circuit(circ, snapshot_type, i,
                                 circ.qubits, conditional=False, pauliop=pauliop)
         time_passed = time
@@ -176,12 +207,12 @@ ax.plot(times, exp_plus, label='<+|X|+>')
 ax.legend()
 # %% plot fid values
 fig, ax = plt.subplots(1, 1, figsize=(8, 6))
-ax.plot(times, fid_0, 'b', label='|0>')
-ax.plot(times, fid_1, 'r', label='|1>')
-ax.plot(times, fid_plus, 'g', label='|+>')
-ax.plot(times, np.exp(-times/T1), '--', label=r'$e^{-t/T_1}$ (|1>)')
+ax.plot(times, fid_0, 'b', label=r'$|0\rangle$')
+ax.plot(times, fid_1, 'r', label=r'$|1\rangle$')
+ax.plot(times, fid_plus, 'g', label=r'$|+\rangle$')
+ax.plot(times, np.exp(-times/T1), '--', label=r'$e^{-t/T_1}$ ($|0\rangle$)')
 ax.plot(times, 1/2+1/2 * np.exp(-times/T2),
-        '--', label=r'$1/2+1/2\cdot e^{(-t/T_2)}$ (|+>)')
+        '--', label=r'$1/2+1/2\cdot e^{(-t/T_2)}$ ($|+\rangle$)')
 ax.legend()
 ax.set_xlabel('Time [ns]')
 ax.set_ylabel('Probability of initial value')
@@ -193,6 +224,8 @@ fid_0 = [state_fidelity(logical_states(include_ancillas=None)[0], res_0.data()['
 fid_0_L = [state_fidelity([1, 0], project_dm_to_logical_subspace_V1(res_0.data()['dm_'+str(index)]))
            for index in range(n_datapoints)]
 exp_0_L = [exp_projected(res_0.data()['dm_'+str(index)]) for index in range(n_datapoints)]
+# res_0_P = get_idle_projected_encoded_513(times, snapshot_type=['exp'], T1=T1, T2=T2)
+# exp_0_P = [res_0_P.data()['exp_'+str(index)]for index in range(n_datapoints)]
 
 res_1 = get_idle_encoded_513(times, snapshot_type=['exp', 'dm'], T1=T1, T2=T2,theta=np.pi)
 exp_1 = [res_1.data()['exp_'+str(index)]for index in range(n_datapoints)]
@@ -274,7 +307,7 @@ ax[0].plot(times, fid_plus, label=r'$|+_L\rangle$')
 ax[0].plot(times, monoExp(times,*pars), '--',label=r'$e^{-t/T}$,'+f' T={T:.0f} ns')
 ax[0].plot(times, times*0+1/16, label=r'$1/16$')
 ax[0].set_xlabel('Time [ns]')
-ax[0].set_ylabel(r'Prob initial state')
+ax[0].set_ylabel(r'Prob of initial state')
 ax[0].legend()
 
 # Projected fid
@@ -304,4 +337,13 @@ F_L_V2 = state_fidelity(logical_states(include_ancillas=None)[0], project_dm_to_
 print('P_L','*','F_L','=','F_phys','?')
 print(P_L,'*',F_L,'=',F_phys)
 print(np.abs(P_L*F_L-F_phys)<0.01)
+# %%
+
+maximally_mixed_state = np.eye(2**5)/2**5
+ket0 = [0]*2**5
+ket0[0] = 1
+
+print(state_fidelity(logical_states(include_ancillas=None)[0],maximally_mixed_state))
+print(state_fidelity(logical_states(include_ancillas=None)[0],ket0))
+
 # %%
