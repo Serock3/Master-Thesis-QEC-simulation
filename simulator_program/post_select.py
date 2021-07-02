60b55698e35a36831d8c6db627b717f3c0d98d8f
Sebastian Holmin <sebastian.holmin@hotmail.com>
2021-07-02 17:00:58 +0200

    Post select whit extra snapshots

--- simulator_program/post_select.py
+++ simulator_program/post_select.py
@@ -11,6 +11,7 @@ from matplotlib import pyplot as plt
 from qiskit import execute, Aer
 from qiskit.aqua.utils import get_subsystems_counts
 from qiskit.quantum_info import state_fidelity
+import numpy as np
 import warnings
 # %%
 """
@@ -24,6 +25,139 @@ TODO:
 4. Make a plot with no processing, post selection and post processing for comparison
 
 """
+
+
+def get_subsystem_counts_up_to_cycle(counts, cycle, hex_keys=False):
+    """Get counts for the subsystem of registers up to cycle. From the reduced dict
+    post selection counts can be extracted. Key is converted to int.
+
+    E.g. for cycle = 0
+    {'00000 00111 00011':1, '00000 00101 00000':4, '00000 00000 00000':10} -> {0:14, 3:1}
+
+    Args:
+        counts (Dict): Counts dictionary
+        cycle (int): current cycle
+    Returns:
+        Dict: subsystem counts. If post_select_syndrome is set then and int is returned
+    """
+
+    subsys_counts = {}
+    for outcome in counts:
+        formated_outcome = int(''.join([key for key in outcome.split()[
+                               len(outcome.split())-(cycle):]]).zfill(1), 2)
+        if hex_keys:
+            formated_outcome = hex(formated_outcome)
+        if formated_outcome in subsys_counts:
+            subsys_counts[formated_outcome] += counts[outcome]
+        else:
+            subsys_counts[formated_outcome] = counts[outcome]
+    return subsys_counts
+
+
+def get_subsystem_counts_up_to_snapshot(counts, snapshot_index, hex_keys=False):
+    """Get counts for the subsystem of registers up to cycle. From the reduced dict
+    post selection counts can be extracted. Key is converted to int.
+
+    E.g. for cycle = 0
+    {'00000 00111 00011':1, '00000 00101 00000':4, '00000 00000 00000':10} -> {0:14, 3:1}
+
+    Args:
+        counts (Dict): Counts dictionary
+        cycle (int): current cycle
+    Returns:
+        Dict: subsystem counts. If post_select_syndrome is set then and int is returned
+    """
+
+    subsys_counts = {}
+    for outcome in counts:
+        trimmed_outcome = outcome.replace(" ", "")
+        formated_outcome = int(
+            trimmed_outcome[len(trimmed_outcome)-snapshot_index:].zfill(1), 2)
+        if hex_keys:
+            formated_outcome = hex(formated_outcome)
+        if formated_outcome in subsys_counts:
+            subsys_counts[formated_outcome] += counts[outcome]
+        else:
+            subsys_counts[formated_outcome] = counts[outcome]
+    return subsys_counts
+
+
+def _expand_key(key: str, current_cycle: int, n_cycles: int):
+    """Expand key into current_cycle copies, e.g. '101'->'101101101'"""
+    value = int((key*current_cycle).zfill(1), 2)
+    num_measurements = (n_cycles+1)*len(key)
+    # For every 64th measurement, the string will be padded with 16 additional zeros
+    hex_padding = int((num_measurements)/64)*16+3
+    return f"{value:#0{hex_padding}x}"
+
+
+def get_trivial_post_select_counts(counts, n_cycles, trivial_key: str = '0000'):
+    # TODO: this if statement is unnecessary if instead we use the default trivial_key = '0000'
+    # if trivial_key is None:
+    #     return [get_subsystem_counts_up_to_cycle(
+    #         counts, current_cycle)[0]
+    #         for current_cycle in range(n_cycles+1)]
+    # else:
+    trivial_key_list = [int(_expand_key(trivial_key, current_cycle, n_cycles), 16)
+                        for current_cycle in range(n_cycles+1)]
+    return [get_subsystem_counts_up_to_cycle(
+        counts, current_cycle)[trivial_key_list[current_cycle]]
+        for current_cycle in range(n_cycles+1)]
+
+
+def get_trivial_post_select_counts_V2(counts, stab_datapoints, num_snapshots):
+    """
+    Supports up to one snapshot per measurement.
+    """
+    trivial_counts = np.zeros(num_snapshots)
+    # TODO: Implement support for other trivial keys?
+    counts_at_stab = np.array([get_subsystem_counts_up_to_snapshot(counts, stab)[0] for stab in range(len(stab_datapoints)+1)])
+    for snap in range(num_snapshots):
+        for j in range(len(stab_datapoints)+1):
+            trivial_counts[snap] = counts_at_stab[j]
+            if snap < ([0]+stab_datapoints)[j]:
+                break
+    return trivial_counts
+
+
+def get_trivial_exp_value(results, n_cycles: int, trivial_key: str = '0000'):
+    """Get the expectation values corresponding to only trivial stabilizer measurements
+    (so far).
+
+    Args:
+        results (Results object): Simulation result
+        n_cycles (int):
+        trivial_key (str, optional): Measurement bitstring corresponding to what a trivial measurement
+        is for the given code, e.g. '101' for the distance two code. Defaults to '0000'.
+
+    Returns:
+        List[float]: List of expectation values post selected for trivial stabilizer outcomes.
+    """
+    trivial_key_list = [_expand_key(trivial_key, current_cycle, n_cycles)
+                        for current_cycle in range(n_cycles+1)]
+    return [results.data()[get_snapshot_label(snapshot_type='exp', conditional=True, current_cycle=current_cycle)][trivial_key]
+            for current_cycle, trivial_key in enumerate(trivial_key_list)]
+
+
+def get_trivial_post_select_den_mat(results, n_cycles: int, trivial_key: str = '0000'):
+    """Get the density matrices corresponding to only trivial stabilizer measurements 
+    (so far).
+
+    Args:
+        results (Results object): Simulation result
+        n_cycles (int): 
+        trivial_key (str, optional): Measurement bitstring corresponding to what a trivial measurement
+        is for the given code, e.g. '101' for the distance two code. Defaults to '0000'.
+
+    Returns:
+        List[np.ndarray]: List of density matrices post selected for trivial stabilizer outcomes.
+    """
+    trivial_key_list = [_expand_key(trivial_key, current_cycle, n_cycles)
+                        for current_cycle in range(n_cycles+1)]
+    return [results.data()[get_snapshot_label(snapshot_type='dm', conditional=True, current_cycle=current_cycle)][trivial_key]
+            for current_cycle, trivial_key in enumerate(trivial_key_list)]
+
+# %% Depricated functions
 
 
 def reformat_density_snapshot(results) -> dict:
@@ -54,91 +188,6 @@ def get_trivial_subsystem_counts_at_cycle(results, n_shots, cycle):
         if int(key) == 0:
             count_trivial_syndrome = syndrome_reg_counts[key]
     return count_trivial_syndrome/n_shots
-
-
-def get_subsystem_counts_up_to_cycle(counts, cycle):
-    """Get counts for the subsystem of registers up to cycle. From the reduced dict
-    post selection counts can be extracted. Key is converted to int.
-
-    E.g. for cycle = 0
-    {'00000 00111 00011':1, '00000 00101 00000':4, '00000 00000 00000':10} -> {0:14, 3:1}
-
-    Args:
-        counts (Dict): Counts dictionary
-        cycle (int): current cycle
-    Returns:
-        Dict: subsystem counts. If post_select_syndrome is set then and int is returned
-    """
-
-    subsys_counts = {}
-    for outcome in counts:
-        formated_outcome = int(''.join([key for key in outcome.split()[
-                               len(outcome.split())-(cycle):]]).zfill(1), 2)
-        if formated_outcome in subsys_counts:
-            subsys_counts[formated_outcome] += counts[outcome]
-        else:
-            subsys_counts[formated_outcome] = counts[outcome]
-    return subsys_counts
-
-
-def _expand_key(key: str, current_cycle: int, n_cycles: int):
-    # Expand key into current_cycle copies, e.g. '101'->'101101101'
-    value = int((key*current_cycle).zfill(1), 2)
-    num_measurements = (n_cycles+1)*len(key)
-    # For every 64th measurement, the string will be padded with 16 additional zeros
-    hex_padding = int((num_measurements)/64)*16+3
-    return f"{value:#0{hex_padding}x}"
-
-
-def get_trivial_post_select_counts(counts, n_cycles, trivial_key: str = '0000'):
-    # TODO: this if statement is unnecessary if instead we use the default trivial_key = '0000'
-    # if trivial_key is None:
-    #     return [get_subsystem_counts_up_to_cycle(
-    #         counts, current_cycle)[0]
-    #         for current_cycle in range(n_cycles+1)]
-    # else:
-    trivial_key_list = [int(_expand_key(trivial_key, current_cycle, n_cycles), 16)
-                        for current_cycle in range(n_cycles+1)]
-    return [get_subsystem_counts_up_to_cycle(
-        counts, current_cycle)[trivial_key_list[current_cycle]]
-        for current_cycle in range(n_cycles+1)]
-
-
-def get_trivial_exp_value(results, n_cycles: int, trivial_key: str = '0000'):
-    """Get the expectation values corresponding to only trivial stabilizer measurements
-    (so far).
-
-    Args:
-        results (Results object): Simulation result
-        n_cycles (int):
-        trivial_key (str, optional): Measurement bitstring corresponding to what a trivial measurement
-        is for the given code, e.g. '101' for the distance two code. Defaults to '0000'.
-
-    Returns:
-        List[float]: List of expectation values post selected for trivial stabilizer outcomes.
-    """
-    trivial_key_list = [_expand_key(trivial_key, current_cycle, n_cycles)
-                        for current_cycle in range(n_cycles+1)]
-    return [results.data()[get_snapshot_label(snapshot_type='exp', conditional=True, current_cycle=current_cycle)][trivial_key]
-            for current_cycle, trivial_key in enumerate(trivial_key_list)]
-
-def get_trivial_post_select_den_mat(results, n_cycles: int, trivial_key: str = '0000'):
-    """Get the density matrices corresponding to only trivial stabilizer measurements 
-    (so far).
-
-    Args:
-        results (Results object): Simulation result
-        n_cycles (int): 
-        trivial_key (str, optional): Measurement bitstring corresponding to what a trivial measurement
-        is for the given code, e.g. '101' for the distance two code. Defaults to '0000'.
-
-    Returns:
-        List[np.ndarray]: List of density matrices post selected for trivial stabilizer outcomes.
-    """
-    trivial_key_list = [_expand_key(trivial_key, current_cycle, n_cycles)
-                        for current_cycle in range(n_cycles+1)]
-    return [results.data()[get_snapshot_label(snapshot_type='dm', conditional=True, current_cycle=current_cycle)][trivial_key]
-            for current_cycle, trivial_key in enumerate(trivial_key_list)]
 
 
 def get_trivial_state(circ):
