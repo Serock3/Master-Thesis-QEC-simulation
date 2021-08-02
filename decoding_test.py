406dfd454f2e038d0986aea008c34d2e20e416f6
Sebastian Holmin <sebastian.holmin@hotmail.com>
2021-08-02 08:41:01 +0200

    Gridplot for decoding errors

--- decoding_test.py
+++ decoding_test.py
@@ -1,4 +1,5 @@
 # %%
+from qiskit.quantum_info.states.measures import state_fidelity
 from simulator_program.idle_noise import add_idle_noise_to_circuit
 from simulator_program.custom_noise_models import standard_times_delay, standard_times
 
@@ -11,15 +12,14 @@ from simulator_program import post_process, post_select
 
 # %% Set up circuit
 kwargs = {
-    'n_cycles': 4,
+    'n_cycles': 1,
     'reset': True,
     'recovery': True,
-    'flag': False,
     'encoding': False,
-    'conditional': False,
+    'conditional': True,
     'include_barriers': True,
     'generator_snapshot': True,
-    'idle_snapshots': 0,
+    'idle_snapshots': 10,
     'final_measure': False,
     'idle_delay': 'before'}
 # Define our registers (Maybe to be written as function?)
@@ -36,7 +36,7 @@ circ, times = add_idle_noise_to_circuit(circ, gate_times, return_time=True)
 
 # %%
 # Run it
-n_shots = 1024*4
+n_shots = 1024*8
 results = default_execute(circ, n_shots, gate_times=gate_times)
 # %%
 logical = logical_states(None)
@@ -65,10 +65,17 @@ def reduce_key(key: str, current_snapshot: int, measurements_per_snapshot=1):
     """
     return hex(int(bin(int(key, 16))[2:][:measurements_per_snapshot*current_snapshot].zfill(1), 2))
 
+def get_stab_datapoints(where = True):
+    snap_indices = np.arange(label_counter.value)
+    snaps_per_cycle =  kwargs['idle_snapshots']+5
+    stab_indices_in_cycle = [i+1 for i in range(4)] if kwargs['idle_delay']=='after' else [i+1+kwargs['idle_snapshots'] for i in range(4)]
+    stab_cond = np.logical_or.reduce(np.array([(snap_indices) % (snaps_per_cycle) == i for i in stab_indices_in_cycle ]), dtype=bool)
+    if where == True:
+        return list(np.where(stab_cond)[0])
+    else:
+        return list(stab_cond)
 
-stab_datapoints = list(np.where(np.logical_or.reduce(np.array([(kwargs['idle_snapshots']+np.arange(label_counter.value)) % (
-        kwargs['idle_snapshots']+5) == kwargs['idle_snapshots'] + i+1 for i in range(4)]), dtype=bool))[0])
-
+stab_datapoints = get_stab_datapoints()
 
 colors = plt.get_cmap("tab10")
 time = np.empty(label_counter.value)
@@ -78,6 +85,8 @@ P_w1 = np.empty(label_counter.value)
 P_w2 = np.empty(label_counter.value)
 P = np.empty(label_counter.value)
 
+# TODO: Make work with delay snapshots for key other than '0x0' 
+# (can't accept measurements less often than once per snapshot)
 key = '0x0'
 for i in range(label_counter.value):
     label = get_snapshot_label('dm', kwargs['conditional'], i)
@@ -85,7 +94,7 @@ for i in range(label_counter.value):
         rho = results.data()[label][reduce_key(key, i)]
     else:
         rho = results.data()[label]
-    time[i] = times[label]/1000
+    time[i] = times[label]
 
     P_0[i] = overlap_with_subspace(rho, logical[0])
     P_1[i] = overlap_with_subspace(rho, logical[1])
@@ -121,14 +130,15 @@ plt.legend(labels=[r'$|0\rangle_L$', 'Weight 1', 'Weight 2', r'$|1\rangle_L$'])
 # %% Look at the proportion of states that get projected to |0>_L from w1 and to |1>_L from w2, are they the same?
 
 
-def get_cycle_index(cycle_num):
-    """Returns index at the end of the cycle"""
-    return np.where((kwargs['idle_snapshots']+np.arange(label_counter.value)) % (kwargs['idle_snapshots']+5) == kwargs['idle_snapshots']+4)[0][cycle_num]
+def get_cycle_indices():
+    """Returns index of the end of each cycle (final stabilizer measurement)."""
+    stab_offset = 0 if kwargs['idle_delay']=='after' else kwargs['idle_snapshots'] 
+    return np.where((np.arange(label_counter.value)) % (kwargs['idle_snapshots']+5) == stab_offset+4)[0]
 
 
 for cycle_num in range(kwargs['n_cycles']):
     print(cycle_num)
-    cycle_index = get_cycle_index(cycle_num)
+    cycle_index = get_cycle_indices()[cycle_num]
     print((P_0[cycle_index+1]-P_0[cycle_index])/P_w1[cycle_index])
     print((P_1[cycle_index+1]-P_1[cycle_index])/P_w2[cycle_index])
     print('')
@@ -137,4 +147,54 @@ for cycle_num in range(kwargs['n_cycles']):
 # %%
 post_select.get_subsystem_counts_up_to_cycle(
     results.get_counts(), 1, hex_keys=True)
+# %% Look at overlap with each state before first correction
+
+if kwargs['conditional']:
+    cycle_end = get_cycle_indices()[0]
+    label = get_snapshot_label('dm', kwargs['conditional'], cycle_end)
+    overlaps = np.zeros((16,32))
+    for key in  results.data()[label]:
+        print(bin(int(key,16))[2:].zfill(4))
+        rho = results.data()[label][key]
+
+        basis = [logical[0],*weight_1,logical[1],*weight_2]
+        overlap = np.empty(32)
+        for i in range(32):
+            overlap[i] = state_fidelity(basis[i],rho)
+        # plt.bar(range(32),overlap)
+        # plt.show()
+        overlaps[int(key,16),:] = overlap
+    threshold = np.zeros(16)
+    for key in range(16):
+        threshold[key] = overlaps[key,key] < 0.5
+
+    fig, ax = plt.subplots(1, 1, figsize=(7, 5))
+    HM =  ax.imshow(overlaps)
+
+    c = 'red'
+    for i in range(16):
+        if threshold[i] == 0:
+            continue
+        else:
+            ax.plot([i-0.5,i+0.5],[i - 0.5,i - 0.5],c)
+            ax.plot([i-0.5,i+0.5],[i + 0.5,i + 0.5],c)
+            ax.plot([i+0.5,i+0.5],[i - 0.5,i + 0.5],c)
+            ax.plot([i-0.5,i-0.5],[i - 0.5,i + 0.5],c)
+
+    ax.set_xticks(np.arange(32))
+    ax.set_yticks(np.arange(16))
+    ax.set_xticklabels([bin(key)[2:].zfill(4) for key in range(16)]+[bin(key)[2:].zfill(4) for key in range(16)])
+    ax.set_yticklabels([bin(key)[2:].zfill(4) for key in range(16)])
+    
+    plt.setp(ax.get_xticklabels(), rotation=90, ha="right",
+         rotation_mode="anchor")
+    cbar0 = fig.colorbar(HM, ax=ax, orientation='horizontal',
+                     fraction=.06, pad=0.25)
+    fig.suptitle("Fidelity to the 32 basis states conditioned on stabilizer measurements\n red squares show where fid<0.5 (is that meaningful?)")
+    ax.set_xlabel('Basis states, labeled by their eigenvalues to the stabilizers\n'+r"Left: weight $\leq$ 1 from $|0\rangle$. Right: weight $\geq$ 2 from $|0\rangle$")
+    ax.set_ylabel("Simualtion state\n conditioned on stabilizer measurements")
+    fig.tight_layout()
+    plt.show()
+else:
+    print('NO CONDITIONAL!')
 # %%
