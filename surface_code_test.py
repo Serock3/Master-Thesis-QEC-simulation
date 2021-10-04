2d1a410087014b660f741e4ba9f310f87e3a3c85
Sebastian Holmin <sebastian.holmin@hotmail.com>
2021-10-04 19:38:40 +0200

    Fix imports

--- surface_code_test.py
+++ surface_code_test.py
@@ -1,23 +1,9 @@
-#%%
-from qiskit import execute
-
-try:
-    from qiskit import Aer
-
-    HAS_AER = True
-except ImportError:
-    from qiskit import BasicAer
-
-    HAS_AER = False
+# %%
+from surface_code.surface_code.circuits import SurfaceCode
+import sys
+from qiskit import execute, Aer
 
 simulator = Aer.get_backend('qasm_simulator')
-
-
-
-import sys
-sys.path.insert(0, '../')
-# TODO: Repo cannot contain "-"!! 
-from surface_code.surface_code.circuits import SurfaceCode
 
 # Set up a d=3, T=3 code
 code = SurfaceCode(3, 3)
@@ -26,15 +12,15 @@ job = execute(code.circuit['0'], simulator)
 raw_results = {}
 raw_results['0'] = job.result().get_counts()
 
-processed_results={}
-processed_results=code.process_results(raw_results['0'])
+processed_results = {}
+processed_results = code.process_results(raw_results['0'])
 
-j=0
+j = 0
 for i in (processed_results):
-    print("results from circuit execution round",j,":", processed_results[2])
-    j=j+1
-    
-nodesX,nodesZ = code.extract_nodes(processed_results)
+    print("results from circuit execution round", j, ":", processed_results[2])
+    j = j+1
+
+nodesX, nodesZ = code.extract_nodes(processed_results)
 print("error nodes in X", nodesX)
 print("error nodes in Z", nodesZ)
 print("No Z error as logical 0 state is an eigenstate of logical Z (given no noise is added to the system)")
